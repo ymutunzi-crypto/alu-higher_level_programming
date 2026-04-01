fc6ce9db954b34d39d10e48311e288732aefe053
ymutunzi-crypto <y.mutunzi@alustudent.com>
2026-04-01 08:12:04 -0700

    Add function that divides elements of two lists with error handling

---
+++ python-exceptions/4-list_division.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+def list_division(my_list_1, my_list_2, list_length):
+    """Divides element by element 2 lists with specific error handling"""
+    new_list = []
+    for i in range(list_length):
+        res = 0
+        try:
+            res = my_list_1[i] / my_list_2[i]
+        except ZeroDivisionError:
+            print("division by 0")
+        except (TypeError, ValueError):
+            print("wrong type")
+        except IndexError:
+            print("out of range")
+        finally:
+            new_list.append(res)
+    return new_list
